508e73e0cb8b6b26100304f156a6893680f08dfa
nesteruk <yurii.nesteruk@gmail.com>
2023-03-30 16:46:25 +0300

    init

---
+++ app/src/main/java/io/neomsoft/ciceronconposenavigation/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.neomsoft.ciceronconposenavigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+        }
+    }
+}
